143fd7d9911c30db310f3be925d79fc003166957
Amandeep Singh <amandeepsingh@Amandeeps-MacBook-Pro.local>
2023-10-06 00:30:10 +0530

    new yarg argv code

--- app.js
+++ app.js
@@ -1,2 +1,26 @@
+const yargs = require('yargs')
+
+
+command = process.argv[2]
+
+if (command == "list") {
+    console.log("this is list")
+}
+
+yargs.command({
+    command: 'list',
+    describe: 'show list names',
+    handler: function () {
+
+        console.log("amandeep " + " singh" + this.yargs)
+    }
+})
+
+console.log(yargs.argv)
+
+
+
+
+
 
 
