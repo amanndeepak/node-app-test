a8b04db835e738b53082ed87a84ebdd0edcda13e
Amandeep Singh <amandeepsingh@Amandeeps-MacBook-Pro.local>
2023-10-06 01:20:54 +0530

    CRUD operaion with the file system from my command

--- app.js
+++ app.js
@@ -4,8 +4,33 @@ const book = {
     title: "ego is the enemy",
     author: "ryan Holiday"
 }
-const mybookJOSN = JSON.stringify(book)
-filesys.writeFileSync("1-Json.json", mybookJOSN)
-const data = filesys.readFileSync('1-Json.json')
-console.log("data is " + (JSON.parse(data.toString())).title)
+
+mycommandv = process.argv[2];
+
+if (mycommandv == "create") {
+    const mybookJOSN = JSON.stringify(book)
+    filesys.writeFileSync("1-Json.json", mybookJOSN)
+    console.log("file created!")
+} else if (mycommandv == "read") {
+    const data = filesys.readFileSync('1-Json.json')
+    console.log("data in file is", data.toString())
+}
+else if (mycommandv == "update") {
+    const data = filesys.readFileSync('1-Json.json')
+    const mydata = JSON.parse(data.toString())
+    mydata.title = "Rich Dad Poor Dad";
+    mydata.author = "Robert Kavasaki"
+    filesys.writeFileSync("1-Json.json", JSON.stringify(mydata))
+    console.log("file updated")
+} else if (mycommandv == "delete") {
+    filesys.unlinkSync("1-Json.json")
+    console.log("file Deleted !")
+} else {
+    console.log("choose \n 1. create\n 2. update\n 3. read \n 4. delete\n");
+}
+
+
+
+
+
 
--- done.js
+++ done.js
@@ -35,30 +35,40 @@ yargs.command({
 console.log(yargs.argv)
 
 
-// file system command to add and remove a file 
+// CRUD operation for file generation using File 
+const filesys = require('fs')
 
-mycommand = process.argv[2];
-
-
-if (mycommand == "add") {
-    fielsystem.writeFileSync("abc.txt", "folder added by user");
-    console.log("file created successfully !! :-) ")
-} else if (mycommand = "remove") {
-    fielsystem.unlinkSync("abc.txt");
-    console.log("file removed successfully !")
-} else {
-    console.log("please give valid command")
-}
-
-
-// json data to the file by strringfy and parse
 const book = {
     title: "ego is the enemy",
     author: "ryan Holiday"
 }
-const mybookJOSN = JSON.stringify(book)
-filesys.writeFileSync("1-Json.json", mybookJOSN)
-const data = filesys.readFileSync('1-Json.json')
-console.log("data is " + (JSON.parse(data.toString())).title)
+
+mycommandv = process.argv[2];
+
+if (mycommandv == "create") {
+    const mybookJOSN = JSON.stringify(book)
+    filesys.writeFileSync("1-Json.json", mybookJOSN)
+    console.log("file created!")
+} else if (mycommandv == "read") {
+    const data = filesys.readFileSync('1-Json.json')
+    console.log("data in file is", data.toString())
+}
+else if (mycommandv == "update") {
+    const data = filesys.readFileSync('1-Json.json')
+    const mydata = JSON.parse(data.toString())
+    mydata.title = "Rich Dad Poor Dad";
+    mydata.author = "Robert Kavasaki"
+    filesys.writeFileSync("1-Json.json", JSON.stringify(mydata))
+    console.log("file updated")
+} else if (mycommandv == "delete") {
+    filesys.unlinkSync("1-Json.json")
+    console.log("file Deleted !")
+} else {
+    console.log("choose \n 1. create\n 2. update\n 3. read \n 4. delete\n");
+}
+
+
+
+
 
 
