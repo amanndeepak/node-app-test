17b0d051320c34aba7e4cda9fe977deaf89e4944
Amandeep Singh <amandeepsingh@Amandeeps-MacBook-Pro.local>
2023-10-06 01:36:03 +0530

    error handler

--- app.js
+++ app.js
@@ -1,36 +1,5 @@
-const filesys = require('fs')
-
-const book = {
-    title: "ego is the enemy",
-    author: "ryan Holiday"
-}
-
-mycommandv = process.argv[2];
-
-if (mycommandv == "create") {
-    const mybookJOSN = JSON.stringify(book)
-    filesys.writeFileSync("1-Json.json", mybookJOSN)
-    console.log("file created!")
-} else if (mycommandv == "read") {
-    const data = filesys.readFileSync('1-Json.json')
-    console.log("data in file is", data.toString())
-}
-else if (mycommandv == "update") {
-    const data = filesys.readFileSync('1-Json.json')
-    const mydata = JSON.parse(data.toString())
-    mydata.title = "Rich Dad Poor Dad";
-    mydata.author = "Robert Kavasaki"
-    filesys.writeFileSync("1-Json.json", JSON.stringify(mydata))
-    console.log("file updated")
-} else if (mycommandv == "delete") {
-    filesys.unlinkSync("1-Json.json")
-    console.log("file Deleted !")
-} else {
-    console.log("choose \n 1. create\n 2. update\n 3. read \n 4. delete\n");
-}
+const notes = require('./notes.js')
 
 
-
-
-
-
+console.log(notes.getNotes())
+console.log(notes.addNotes("rich dad poor dad", " by robert kavasaki"))
--- done.js
+++ done.js
@@ -69,6 +69,53 @@ else if (mycommandv == "update") {
 
 
 
+// error handline 
+
+const filesys = require('fs')
+
+const book = {
+    title: "ego is the enemy",
+    author: "ryan Holiday"
+}
+
+mycommandv = process.argv[2];
+
+if (mycommandv == "create") {
+    const mybookJOSN = JSON.stringify(book)
+    filesys.writeFileSync("1-Json.json", mybookJOSN)
+    console.log("file created !")
+} else if (mycommandv == "read") {
+    const data = filesys.readFileSync('1-Json.json')
+    console.log("data in file is ", data.toString())
+}
+else if (mycommandv == "update") {
+    const data = filesys.readFileSync('1-Json.json')
+    const mydata = JSON.parse(data.toString())
+    mydata.title = "Rich Dad Poor Dad";
+    mydata.author = "Robert Kavasaki"
+    filesys.writeFileSync("1-Json.json", JSON.stringify(mydata))
+    console.log("file updated !")
+} else if (mycommandv == "delete") {
+    try {
+        filesys.unlinkSync("1-Json.json")
+        console.log("file Deleted !")
+    } catch (e) {
+        console.log("you are trying to delete file which is not created ! Ha Ha Ha ");
+    }
+} else {
+    console.log("choose \n 1. create\n 2. update\n 3. read \n 4. delete\n");
+}
+
+//add notes from another file 
+
+const notes = require('./notes.js')
+
+
+console.log(notes.getNotes())
+console.log(notes.addNotes("rich dad poor dad", " by robert kavasaki"))
+
+
+
 
 
 
